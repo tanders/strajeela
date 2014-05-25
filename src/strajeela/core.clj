959a7d5081f495ed49dc391c8e4d9c6d50cee0d7
Torsten Anders <torsten.anders@beds.ac.uk>
2014-05-25 23:45:34 +0100

    Emptied strajeela.core for now

--- src/strajeela/core.clj
+++ src/strajeela/core.clj
@@ -1,16 +1,4 @@
 (ns strajeela.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
 
-(defn plus1
-  "This is just some stupid test :)"
-  [x]
-  (+ 1  x))
-
-(plus1 3/2)
-
-(def test '(1 2 3 4))
