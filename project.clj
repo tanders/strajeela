77e051010ff71b4264a92516c69582a0181e9b90
Torsten Anders <torsten.anders@beds.ac.uk>
2014-05-25 23:52:16 +0100

    Doc generation by codox added and customised

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject strajeela "0.0.1-SNAPSHOT"
-  :description "This software is intended to be a successor of Strasheela at some stage -- for the original see strasheela.sourceforge.net/"
+  :description "This software is intended to be a successor of Strasheela at some stage -- for the original see http://strasheela.sourceforge.net/"
   ;; :url "http://example.com/FIXME"
   :license {:name "GNU General Public License"
             :url "https://gnu.org/licenses/gpl.html"}
@@ -7,7 +7,13 @@
                  ;; an alternative class-based object-system for Clojure based on hash-maps and multimethods
                  ;; Doc: https://github.com/eduardoejp/fenrir, http://eduardoejp.github.io/fenrir/fenrir-api.html
                  ;; [fenrir "0.1.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  ]
+  :codox {:defaults {:doc "TODO: write docs"
+                     :doc/format :markdown}
+          :output-dir "doc/reference"
+          :src-dir-uri "https://github.com/tanders/strajeela/blob/master/"
+          :src-linenum-anchor-prefix "L"}
   :main ^:skip-aot strajeela.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
