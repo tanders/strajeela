f727f119bc1648e5a5f856d1cfd9234ae29e4ac2
Torsten Anders <torsten.anders@beds.ac.uk>
2014-09-01 19:16:53 +0100

    Added clojure2minizinc to project

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "GNU General Public License"
             :url "https://gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [minizinc/clojure2minizinc "0.1.0-SNAPSHOT"]
                  ;; an alternative class-based object-system for Clojure based on hash-maps and multimethods
                  ;; Doc: https://github.com/eduardoejp/fenrir, http://eduardoejp.github.io/fenrir/fenrir-api.html
                  ;; [fenrir "0.1.0"]
@@ -14,6 +15,7 @@
           :output-dir "doc/reference"
           :src-dir-uri "https://github.com/tanders/strajeela/blob/master/"
           :src-linenum-anchor-prefix "L"}
-  :main ^:skip-aot strajeela.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  ;; :main ^:skip-aot strajeela.core
+  ;; :target-path "target/%s"
+  ;; :profiles {:uberjar {:aot :all}}
+  )
--- src/strajeela/core.clj
+++ src/strajeela/core.clj
@@ -1,4 +1,3 @@
 (ns strajeela.core
   (:gen-class))
 
-
