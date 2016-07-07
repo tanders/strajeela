13167456698335f5bc7d0b7af50df4d62b8a7ec7
Torsten Anders <torsten.anders@beds.ac.uk>
2016-07-07 23:45:35 +0100

    First version of music representation added (incomplete draft)

--- src/strajeela/score.clj
+++ src/strajeela/score.clj
@@ -1,8 +1,165 @@
 ;;; File defines the music repesentation
 
 (ns strajeela.score
-  ;; (:gen-class)
+  (:require [clojure2minizinc.core :as mz]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; First step: direct translation of 2-part counterpoint example at
+;; strajeela/MiniZinc-sketching/MusicRepresentation/SimpleCounterpoint-twoVoices.mzn
+;; Later I refactor this, so that
+;; - The score definition is defined by a single function -- which users can overwrite to change the default score creation
+;; - A single abstraction creates an arbitrary number of parts
+;; - The Clojure data is stored in some data structure for rel. convenient constraint application -- lets see how that works
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Include statements
+;;;
+
+(mz/include "globals.mzn")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; TODO: Predicate definitions
+;;;
+
+;; !! TODO: perhaps I better define MiniZinc predicates, which add type information
+(comment
+  ;; Constrains the elements of array 'y' to be all different except those
+  ;; elements that are assigned to 'x'.
+  ;; source: modification of MiniZinc's alldifferent_except_0.mzn
+  ;; unused after all
+  (mz/def-submodel alldifferent_except_x
+    [x y]
+    (mz/forall [i (mz/index_set y)
+                j (mz/index_set y)
+                :where (mz/!= i j)]
+      (mz/-> (mz/and (mz/!= (mz/nth y i) x)
+                     (mz/!= (mz/nth y j) x))
+             (mz/!= (mz/nth y i) (mz/nth y j)))))
+  
+  (print (alldifferent_except_x 0 event_duration))
+
   )
+
+(comment 
+
+  (mz/aggregate
+   )
+
+  (mz/aggregate [i (mz/-- 1 3)] 
+    (mz/* i i))
+  ;; means [1*1, 2*2, 3*3]  (in MiniZinc syntax)
+
+
+  (def a (mz/array (mz/-- 1 3) :int))
+  (mz/aggregate [i (mz/-- 1 3)
+                 j (mz/-- 1 3) 
+                 :where (mz/< i j)]
+    (mz/!= (mz/nth a i) (mz/nth a j)))  
+  ;; means [a[1] != a[2], a[2] != a[3]] (in MiniZinc syntax)
+
+
+  )
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Variables
+;;; !! First declared without let etc for testing !!  Refactor later with let or similar 
+;;;
+
+;; 
+;; General variables 
+;;
+
+;; ? Number of time slices? Can be CSP arg.
+(def n (mz/int 'n 20))
+
+;; Can be CSP arg.
+;; Could be defined with ratios in Clojure and automatically translated into integers for CSP by setting the int that represents a 1/4 note, similar to Strasheela
+(def slice_duration_domain #{1}) 
+(def slice_duration (mz/array (mz/-- 1 n) [:var slice_duration_domain]
+                              'slice_duration))
+(def max_slice_dur (mz/int 'max_slice_dur (apply max slice_duration_domain)))
+(def max_end_time (mz/int 'max_end_time (mz/* n max_slice_dur)))
+(def slice_start (mz/array (mz/-- 1 n) [:var (mz/-- 1 max_end_time)]
+                           'slice_start))
+  
+
+;; 
+;; Part-specific variables -- for a single voice
+;;
+
+(defn partify
+  "Generates a part-specific variable name by concatenating a general name and a part number"
+  [var-name part-num]
+  (clojure.string/join [var-name "_" part-num])
+  ;; (symbol (clojure.string/join [var-name "_" part-num]))
+  )
+
+(def part_number 1)
+;; marker where new note starts
+;; new_event/new_event_fd: I need Boolean representation for many constraints, and integer representation for branching
+(def new_event (mz/array (mz/-- 1 n) [:var :bool]
+                         (partify 'new_event part_number)))
+(def new_event_fd (mz/array (mz/-- 1 n) [:var (mz/-- 0 1)]
+                            (partify 'new_event_fd part_number)))
+;; set of indices where events start
+(def event_index_set (mz/variable [:set (mz/-- 1 n)]
+                                  (partify 'event_index_set part_number)))
+;; event start indices without intermediate elements (e.g., consecutive indices are always pointing to different events)
+;; !! n+1 to mark indices beyond the range
+(def event_index (mz/array (mz/-- 1 n) [:var (mz/-- 1 (mz/+ n 1))]
+                           (partify 'event_index part_number)))
+;; start of most recently started note (not necessarily still playing)
+(def recentStartTime (mz/array (mz/-- 1 n) [:var (mz/-- 0 max_end_time)]
+                               (partify 'recentStartTime part_number)))
+;; accumulator for duration: value before new_event=true correct event_duration
+;; !! TODO: can duration be 0, e.g., for grace notes?
+(def event_duration_accum (mz/array (mz/-- 1 n) [:var (mz/-- 1 max_end_time)]
+                               (partify 'event_duration_accum part_number)))
+(def event_duration (mz/array (mz/-- 1 n) [:var (mz/-- 1 max_end_time)]
+                              (partify 'event_duration part_number)))
+;; MIDI note numbers or later other pitch units
+;; Test: restrict pitch domain directly to diatonic scale
+(def pitch_domain #{48, 50, 52, 53, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72})
+(def event_pitch (mz/array (mz/-- 1 n) [:var pitch_domain]
+                           (partify 'event_pitch part_number)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; TODO: Constraints
+;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; TODO: TMP: Example-specific (simple counterpoint) constraints
+;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; TODO: TMP: tests
+;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; TODO: Solve and output
+;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; Old sketches
+;;;
 
 (comment
 
@@ -60,6 +217,11 @@
 
   )
 
+
+(comment
+
+
+  
 ;; Expect a partial score definition and returns a full score with all details. 
 ;; Similar in principle to translation of partial score into full score in Strasheela, but full score is 
 ;; - Not nested objects, but instead Clojure sequences, sets etc (but for which a custom interface is defined)
@@ -70,8 +232,6 @@
   "TODO: def and doc-string"
   [arg-list]
   )
-
-(comment
   
 ;; TODO: 
 (defn export-kern 
